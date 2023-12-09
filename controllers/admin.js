3800ce49f4b88830d90033035ac12e12b5026170
Ali Magdi <37837958+alimagdi12@users.noreply.github.com>
2023-12-09 03:48:45 +0200

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -130,16 +130,16 @@ exports.postDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.postUpload =(req, res) => {
-  // Get the file that was set to our field named "image"
-  const { image } = req.files;
+// exports.postUpload =(req, res) => {
+//   // Get the file that was set to our field named "image"
+//   const { image } = req.files;
 
-  // If no image submitted, exit
-  if (!image) return res.sendStatus(400);
+//   // If no image submitted, exit
+//   if (!image) return res.sendStatus(400);
 
-  // Move the uploaded image to our upload folder
-  image.mv(__dirname + './upload/' + image.name);
+//   // Move the uploaded image to our upload folder
+//   image.mv(__dirname + './upload/' + image.name);
 
-  res.sendStatus(200);
-  res.redirect('/admin/add-product')
-}
+//   res.sendStatus(200);
+//   res.redirect('/admin/add-product')
+// }
