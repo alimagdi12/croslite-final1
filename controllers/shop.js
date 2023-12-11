2771fce553d6f8d0c021bcf8be47616b33a1f7bb
alimagdi12 <alimagdi12367@yahoo.com>
2023-12-11 23:55:00 +0200

    Final Changes

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,19 +16,45 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+// exports.getProduct = (req, res, next) => {
+//   const prodId = req.params.productId;
+//   Product.findById(prodId)
+//     .then(product => {
+//       res.render('shop/product-detail', {
+//         product: product,
+//         pageTitle: product.title,
+//         path: '/products'
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
+
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  // if (!mongoose.Types.ObjectId.isValid(prodId)) {
+  //   // Handle the case when prodId is not a valid ObjectId
+  //   return res.status(400).send('Invalid product ID');
+  // }
+
   Product.findById(prodId)
     .then(product => {
+      if (!product) {
+        // Handle the case when no product is found with the given prodId
+        return res.status(404).send('Product not found');
+      }
+
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
         path: '/products'
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      // Handle other errors that may occur during the query
+      res.status(500).send('An error occurred');
+    });
 };
-
 
 exports.postSearch = (req,res,next)=>{
   const search = req.body.search;
